0ded7e0535ac8990c756ccba9764012ec22ce6c2
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-11-23 15:50:29 -0700

    initial commit

---
+++ Spwifiy/SpwifiyApp.swift
@@ -0,0 +1,20 @@
+//
+//  SpwifiyApp.swift
+//  Spwifiy
+//
+//  Created by Peter Duanmu on 11/23/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SpwifiyApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+    
+}
---
+++ Spwifiy/Views/HomeView.swift
@@ -0,0 +1,15 @@
+//
+//  HomeView.swift
+//  Spwifiy
+//
+//  Created by Peter Duanmu on 11/23/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
