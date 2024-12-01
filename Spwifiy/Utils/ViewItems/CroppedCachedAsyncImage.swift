cafdafdfa9e323dfc3e957f593ff922ed0102c7e
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-12-01 00:06:57 -0700

    update cached async image

--- Spwifiy/Utils/ViewItems/CroppedCachedAsyncImage.swift
+++ Spwifiy/Utils/ViewItems/CroppedCachedAsyncImage.swift
@@ -12,11 +12,8 @@ struct CroppedCachedAsyncImage<GenericShape: Shape>: View {
 
     var url: URL?
 
-    var width: CGFloat?
-    var height: CGFloat?
-
-    var maxWidth: CGFloat?
-    var maxHeight: CGFloat?
+    var width: CGFloat
+    var height: CGFloat
 
     var alignment: Alignment
 
@@ -25,40 +22,27 @@ struct CroppedCachedAsyncImage<GenericShape: Shape>: View {
     var onLoadTask: ((Image) -> Void)?
 
     var body: some View {
-        generateCroppedImage()
-    }
-
-    private func generateCroppedImage() -> AnyView {
-        var image: any View = CachedAsyncImage(url: url, urlCache: .imageCache) { phase in
-                switch phase {
-                case .empty:
-                    ProgressView()
-                        .progressViewStyle(.circular)
-                        .controlSize(.small)
-                case .success(let image):
-                    image
-                        .resizable()
-                        .task {
-                            onLoadTask?(image)
-                        }
-                default:
-                    Image("spwifiy.profile.default")
-                        .resizable()
-                }
+        CachedAsyncImage(url: url, urlCache: .imageCache) { phase in
+            switch phase {
+            case .empty:
+                ProgressView()
+                    .progressViewStyle(.circular)
+                    .controlSize(.small)
+            case .success(let image):
+                image
+                    .resizable()
+                    .task {
+                        onLoadTask?(image)
+                    }
+            default:
+                Image("spwifiy.profile.default")
+                    .resizable()
             }
-            .scaledToFill()
-
-        if let width = width, let height = height {
-            image = image.frame(width: width, height: height, alignment: alignment)
-        } else if let maxWidth = maxWidth, let maxHeight = maxHeight {
-            image = image.frame(maxWidth: maxWidth, maxHeight: maxHeight, alignment: alignment)
         }
-
-        return AnyView(
-            image
-                .clipped()
-                .clipShape(clipShape)
-        )
+        .scaledToFill()
+        .frame(width: width, height: height, alignment: alignment)
+        .clipped()
+        .clipShape(clipShape)
     }
 
 }
