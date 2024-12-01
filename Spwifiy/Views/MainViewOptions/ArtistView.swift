55c762c23a0c1be2ecdb0ff93ed3d0249b0113ce
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2024-12-01 01:28:41 -0700

    initial parallax effect

--- Spwifiy/Views/MainViewOptions/ArtistView.swift
+++ Spwifiy/Views/MainViewOptions/ArtistView.swift
@@ -20,69 +20,132 @@ struct ArtistView: View {
     }
 
     var body: some View {
-        GeometryReader { geom in
+        ScrollView {
             VStack {
-                ZStack {
-                    Group {
-                        if let backgroundImageURL = artistViewModel.backgroundImageURL {
-                            CroppedCachedAsyncImage(url: backgroundImageURL,
-                                                    width: geom.size.width,
-                                                    height: 400,
-                                                    alignment: .top,
-                                                    clipShape: RoundedRectangle(cornerRadius: 5))
-                        } else {
-                            CroppedCachedAsyncImage(url: artistViewModel.artist.images?.first?.url,
-                                                    width: geom.size.width,
-                                                    height: 400,
-                                                    alignment: .center,
-                                                    clipShape: RoundedRectangle(cornerRadius: 5))
+                GeometryReader { geom in
+                    ZStack {
+                        Group {
+                            if let backgroundImageURL = artistViewModel.backgroundImageURL {
+                                CroppedCachedAsyncImage(url: backgroundImageURL,
+                                                        width: geom.size.width,
+                                                        height: 400,
+                                                        alignment: .top,
+                                                        clipShape: RoundedRectangle(cornerRadius: 5))
+                            } else {
+                                CroppedCachedAsyncImage(url: artistViewModel.artist.images?.first?.url,
+                                                        width: geom.size.width,
+                                                        height: 400,
+                                                        alignment: .center,
+                                                        clipShape: RoundedRectangle(cornerRadius: 5))
+                            }
                         }
-                    }
-                    .overlay(
-                        Rectangle()
-                            .foregroundStyle(
-                                LinearGradient(colors: [.clear, .clear, .bgSecondary.opacity(0.8)],
-                                               startPoint: .top,
-                                               endPoint: .bottom)
-                            )
-                    )
+                        .overlay(
+                            Rectangle()
+                                .foregroundStyle(
+                                    LinearGradient(colors: [.clear, .clear, .bgSecondary.opacity(0.8)],
+                                                   startPoint: .top,
+                                                   endPoint: .bottom)
+                                )
+                        )
 
-                    VStack {
-                        Spacer()
+                        VStack {
+                            Spacer()
 
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(artistViewModel.artist.name)
-                                    .font(.satoshiBlack(60))
-                                    .fontWeight(.black)
-                                    .foregroundStyle(.fgPrimary)
+                            HStack(alignment: .bottom) {
+                                VStack(alignment: .leading) {
+                                    Text(artistViewModel.artist.name)
+                                        .font(.satoshiBlack(60))
+                                        .fontWeight(.black)
+                                        .foregroundStyle(.fgPrimary)
+
+                                    Spacer()
+                                        .frame(height: 25)
+
+                                    HStack {
+                                        if let monthlyListeners = artistViewModel.monthlyListeners {
+                                            Text(monthlyListeners.formatted())
+                                        } else {
+                                            ProgressView()
+                                        }
+
+                                        Text("monthly listeners")
+                                    }
+                                    .font(.title2)
+                                }
 
                                 Spacer()
-                                    .frame(height: 25)
 
-                                HStack {
-                                    if let monthlyListeners = artistViewModel.monthlyListeners {
-                                        Text(monthlyListeners.formatted())
-                                    } else {
-                                        ProgressView()
+                                HStack(spacing: 15) {
+                                    Button {
+
+                                    } label: {
+                                        Image("spwifiy.play.fill")
+                                            .resizable()
+                                            .frame(width: 40, height: 40)
                                     }
+                                    .buttonStyle(.plain)
+                                    .cursorHover(.pointingHand)
 
-                                    Text("monthly listeners")
+                                    Button {
+
+                                    } label: {
+                                        Text("Follow")
+                                            .font(.satoshiLight(12))
+                                            .padding()
+                                            .overlay {
+                                                RoundedRectangle(cornerRadius: 40)
+                                                    .foregroundStyle(.fgPrimary.opacity(0.3))
+                                                    .allowsHitTesting(false)
+                                            }
+                                    }
+                                    .buttonStyle(.plain)
+                                    .cursorHover(.pointingHand)
+
+                                    Button {
+
+                                    } label: {
+                                        Image("spwifiy.add.playlist")
+                                            .resizable()
+                                            .frame(width: 40, height: 40)
+                                    }
+                                    .buttonStyle(.plain)
+                                    .cursorHover(.pointingHand)
+
+                                    Button {
+
+                                    } label: {
+                                        Image("spwifiy.add.queue")
+                                            .resizable()
+                                            .frame(width: 40, height: 40)
+                                    }
+                                    .buttonStyle(.plain)
+                                    .cursorHover(.pointingHand)
+
+                                    Button {
+
+                                    } label: {
+                                        Image("spwifiy.more")
+                                            .resizable()
+                                            .frame(width: 40, height: 40)
+                                    }
+                                    .buttonStyle(.plain)
+                                    .cursorHover(.pointingHand)
                                 }
-                                .font(.title2)
                             }
+                            .foregroundStyle(.fgPrimary)
                             .padding()
 
                             Spacer()
+                                .frame(height: 20)
                         }
-
-                        Spacer()
-                            .frame(height: 20)
                     }
+                    .offset(y: -geom.frame(in: .global).minY / 3)
                 }
-                .fixedSize()
+                .frame(height: 400)
 
-                Spacer()
+                Rectangle()
+                    .foregroundStyle(.bgMain)
+                    .frame(height: 1000)
             }
         }
         .task {
