75be84c2faf167a3477b37aa1032f7bff7fd61ea
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2025-03-21 17:56:59 -0600

    move `DisplayType` enum to separate file

---
+++ Spwifiy/Models/DisplayType.swift
@@ -0,0 +1,10 @@
+//
+//  DisplayType.swift
+//  Spwifiy
+//
+//  Created by Peter Duanmu on 3/21/25.
+//
+
+enum DisplayType: String {
+    case list, grid
+}
--- Spwifiy/ViewModels/LikedSongsViewModel.swift
+++ Spwifiy/ViewModels/LikedSongsViewModel.swift
@@ -10,10 +10,6 @@ import SpotifyWebAPI
 
 class LikedSongsViewModel: GenericSongCollectionViewModel {
 
-    public enum DisplayType {
-        case list, grid
-    }
-
     @Published var displayType: DisplayType = .list
 
     override init(spotifyCache: SpotifyCache) {
