d21bb98a444595f9f130607254a804454cdb777b
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2025-03-29 17:40:21 -0600

    fix prev song skip

--- Spwifiy/Extensions/Internal/AVAudioPlayer+songControl.swift
+++ Spwifiy/Extensions/Internal/AVAudioPlayer+songControl.swift
@@ -71,7 +71,7 @@ extension AVAudioPlayer {
     }
 
     public func prevSong() {
-        if currentPlayTime > 0.1 {
+        if currentPlayTime / totalRunTime > 0.01 {
             player.seek(to: CMTime(seconds: 0, preferredTimescale: 1))
         } else {
             updateSong(incBy: -1)
