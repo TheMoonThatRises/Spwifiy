2ae1c4b20d99d28f8cb8bb6cf2cda3f173c4f83b
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2025-03-30 15:21:38 -0600

    make album list lazy

--- Spwifiy/Views/MainViewOptions/ArtistElements/ArtistAlbumView.swift
+++ Spwifiy/Views/MainViewOptions/ArtistElements/ArtistAlbumView.swift
@@ -80,128 +80,130 @@ struct ArtistAlbumListView: View {
     }
 
     var body: some View {
-        ForEach(filteredAlbums, id: \.id) { album in
-            Button {
-                selectedAlbum = album
-            } label: {
-                HStack {
-                    AlbumItemView(album: album, imageOnly: true)
-                        .contentShape(.rect)
-                        .padding()
+        LazyVStack {
+            ForEach(filteredAlbums, id: \.id) { album in
+                Button {
+                    selectedAlbum = album
+                } label: {
+                    HStack {
+                        AlbumItemView(album: album, imageOnly: true)
+                            .contentShape(.rect)
+                            .padding()
 
-                    VStack(alignment: .leading) {
-                        Text(album.name)
-                            .font(.satoshiBlack(20))
-                            .fontWeight(.black)
-                            .foregroundStyle(.fgPrimary)
+                        VStack(alignment: .leading) {
+                            Text(album.name)
+                                .font(.satoshiBlack(20))
+                                .fontWeight(.black)
+                                .foregroundStyle(.fgPrimary)
 
-                        HStack {
-                            Text(album.releaseDate?.formatted(.dateTime.year()) ?? "Unknown year")
+                            HStack {
+                                Text(album.releaseDate?.formatted(.dateTime.year()) ?? "Unknown year")
 
-                            Circle()
-                                .frame(width: 3, height: 3)
-
-                            Text("\(album.totalTracks ?? 0) songs")
-
-                            if let duration = duration(album) {
                                 Circle()
                                     .frame(width: 3, height: 3)
 
-                                Text("\(duration.hours + duration.days * 24) hr \(duration.minutes) min")
+                                Text("\(album.totalTracks ?? 0) songs")
+
+                                if let duration = duration(album) {
+                                    Circle()
+                                        .frame(width: 3, height: 3)
+
+                                    Text("\(duration.hours + duration.days * 24) hr \(duration.minutes) min")
+                                }
                             }
+                            .font(.caption)
+
+                            //                    HStack {
+                            //                        Button {
+                            //                            if avAudioPlayer.playingId == album.id {
+                            //                                avAudioPlayer.togglePlay()
+                            //                            } else {
+                            //                                avAudioPlayer.updatePlayingList(newPlayingId: album.id,
+                            //                                                                tracks: album.tracks?.items ?? [])
+                            //                            }
+                            //                        } label: {
+                            //                            Image(
+                            //                                avAudioPlayer.playingId == album.id &&
+                            //                                avAudioPlayer.isPlaying ? "spwifiy.pause.fill"  : "spwifiy.play.fill"
+                            //                            )
+                            //                            .resizable()
+                            //                            .overlay {
+                            //                                if album.tracks?.items.isEmpty ?? true {
+                            //                                    ProgressView()
+                            //                                        .progressViewStyle(.circular)
+                            //                                }
+                            //                            }
+                            //                            .frame(width: 40, height: 40)
+                            //                        }
+                            //                        .buttonStyle(.plain)
+                            //                        .cursorHover(.pointingHand)
+                            //
+                            //                        DotButton(toggle: $avAudioPlayer.isShuffled,
+                            //                                  image: Image("spwifiy.shuffle"))
+                            //
+                            //                        Button {
+                            //
+                            //                        } label: {
+                            //                            Image("spwifiy.add")
+                            //                                .resizable()
+                            //                                .frame(width: 40, height: 40)
+                            //                        }
+                            //                        .buttonStyle(.plain)
+                            //                        .cursorHover(.pointingHand)
+                            //
+                            //                        Button {
+                            //
+                            //                        } label: {
+                            //                            Image("spwifiy.add.queue")
+                            //                                .resizable()
+                            //                                .frame(width: 40, height: 40)
+                            //                        }
+                            //                        .buttonStyle(.plain)
+                            //                        .cursorHover(.pointingHand)
+                            //                        Button {
+                            //
+                            //                        } label: {
+                            //                            Image("spwifiy.download")
+                            //                                .resizable()
+                            //                                .frame(width: 40, height: 40)
+                            //                        }
+                            //                        .buttonStyle(.plain)
+                            //                        .cursorHover(.pointingHand)
+                            //
+                            //                        Button {
+                            //
+                            //                        } label: {
+                            //                            Image("spwifiy.share")
+                            //                                .resizable()
+                            //                                .frame(width: 40, height: 40)
+                            //                        }
+                            //                        .buttonStyle(.plain)
+                            //                        .cursorHover(.pointingHand)
+                            //
+                            //                        Button {
+                            //
+                            //                        } label: {
+                            //                            Image("spwifiy.more")
+                            //                                .resizable()
+                            //                                .frame(width: 40, height: 40)
+                            //                        }
+                            //                        .buttonStyle(.plain)
+                            //                        .cursorHover(.pointingHand)
+                            //                    }
                         }
-                        .font(.caption)
 
-    //                    HStack {
-    //                        Button {
-    //                            if avAudioPlayer.playingId == album.id {
-    //                                avAudioPlayer.togglePlay()
-    //                            } else {
-    //                                avAudioPlayer.updatePlayingList(newPlayingId: album.id,
-    //                                                                tracks: album.tracks?.items ?? [])
-    //                            }
-    //                        } label: {
-    //                            Image(
-    //                                avAudioPlayer.playingId == album.id &&
-    //                                avAudioPlayer.isPlaying ? "spwifiy.pause.fill"  : "spwifiy.play.fill"
-    //                            )
-    //                            .resizable()
-    //                            .overlay {
-    //                                if album.tracks?.items.isEmpty ?? true {
-    //                                    ProgressView()
-    //                                        .progressViewStyle(.circular)
-    //                                }
-    //                            }
-    //                            .frame(width: 40, height: 40)
-    //                        }
-    //                        .buttonStyle(.plain)
-    //                        .cursorHover(.pointingHand)
-    //
-    //                        DotButton(toggle: $avAudioPlayer.isShuffled,
-    //                                  image: Image("spwifiy.shuffle"))
-    //
-    //                        Button {
-    //
-    //                        } label: {
-    //                            Image("spwifiy.add")
-    //                                .resizable()
-    //                                .frame(width: 40, height: 40)
-    //                        }
-    //                        .buttonStyle(.plain)
-    //                        .cursorHover(.pointingHand)
-    //
-    //                        Button {
-    //
-    //                        } label: {
-    //                            Image("spwifiy.add.queue")
-    //                                .resizable()
-    //                                .frame(width: 40, height: 40)
-    //                        }
-    //                        .buttonStyle(.plain)
-    //                        .cursorHover(.pointingHand)
-    //                        Button {
-    //
-    //                        } label: {
-    //                            Image("spwifiy.download")
-    //                                .resizable()
-    //                                .frame(width: 40, height: 40)
-    //                        }
-    //                        .buttonStyle(.plain)
-    //                        .cursorHover(.pointingHand)
-    //
-    //                        Button {
-    //
-    //                        } label: {
-    //                            Image("spwifiy.share")
-    //                                .resizable()
-    //                                .frame(width: 40, height: 40)
-    //                        }
-    //                        .buttonStyle(.plain)
-    //                        .cursorHover(.pointingHand)
-    //
-    //                        Button {
-    //
-    //                        } label: {
-    //                            Image("spwifiy.more")
-    //                                .resizable()
-    //                                .frame(width: 40, height: 40)
-    //                        }
-    //                        .buttonStyle(.plain)
-    //                        .cursorHover(.pointingHand)
-    //                    }
+                        Spacer()
+
+                        Image("spwifiy.right")
+                            .resizable()
+                            .frame(width: 40, height: 40)
                     }
-
-                    Spacer()
-
-                    Image("spwifiy.right")
-                        .resizable()
-                        .frame(width: 40, height: 40)
+                    .foregroundStyle(.fgSecondary)
                 }
-                .foregroundStyle(.fgSecondary)
+                .buttonStyle(.plain)
+                .cursorHover(.pointingHand)
             }
         }
-        .buttonStyle(.plain)
-        .cursorHover(.pointingHand)
     }
 
 }
