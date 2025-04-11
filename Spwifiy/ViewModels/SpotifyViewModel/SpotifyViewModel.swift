3eae4a14786a708ac14a4182cb1cd226a2082110
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2025-04-10 18:30:10 -0600

    remove auth code len check

--- Spwifiy/ViewModels/SpotifyViewModel/SpotifyViewModel.swift
+++ Spwifiy/ViewModels/SpotifyViewModel/SpotifyViewModel.swift
@@ -12,6 +12,8 @@ import KeychainAccess
 
 class SpotifyViewModel: ObservableObject {
 
+//    private static let authCodeLengths: [Int] = [376, 378, 383]
+
     public enum AuthorizationStatus {
         case none, valid, failed
     }
@@ -129,15 +131,17 @@ class SpotifyViewModel: ObservableObject {
 
     @MainActor
     private func authClient(authResponse: SpotifyAuthResponse) {
-        if ![376, 378].contains(authResponse.accessToken.count) {
-            Task {
-                try? keychain.remove(SpotifyAuthManager.authAccessResponse)
+        print("auth code len: \(authResponse.accessToken.count)")
 
-                await attemptSpotifyAuthToken(method: .`init`)
-            }
-
-            return
-        }
+//        if !SpotifyViewModel.authCodeLengths.contains(authResponse.accessToken.count) {
+//            Task {
+//                try? keychain.remove(SpotifyAuthManager.authAccessResponse)
+//
+//                await attemptSpotifyAuthToken(method: .`init`)
+//            }
+//
+//            return
+//        }
 
         let expirationDate = Date(millisecondsSince1970: authResponse.accessTokenExpirationTimestampMs)
 
